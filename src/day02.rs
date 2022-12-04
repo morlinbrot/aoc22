bae19902f123f7a9db8eda278b2c54798236850c
morlinbrot <morlinbrot@mailbox.org>
2022-12-04 02:17:22 +0100

    Day 2 - part 1

--- src/day02.rs
+++ src/day02.rs
@@ -1,37 +1,49 @@
-use std::cmp::Ordering;
-
 // use crate::util;
 
 // A - X - Rock - 1
 // B - Y - Paper - 2
 // C - Z - Scissors - 3
 
+#[derive(Debug)]
+pub enum Mv {
+    Rck,
+    Ppr,
+    Scs,
+}
+
+impl Mv {
+    fn against(&self, my_mv: &Mv) -> usize {
+        match (self, my_mv) {
+            (Mv::Rck, Mv::Ppr) | (Mv::Ppr, Mv::Scs) | (Mv::Scs, Mv::Rck) => 6,
+            (Mv::Rck, Mv::Scs) | (Mv::Ppr, Mv::Rck) | (Mv::Scs, Mv::Ppr) => 0,
+            _ => 3,
+        }
+    }
+
+    fn val(&self) -> usize {
+        match self {
+            Mv::Rck => 1,
+            Mv::Ppr => 2,
+            Mv::Scs => 3,
+        }
+    }
+}
+
 pub fn part_one() -> usize {
     let str = std::fs::read_to_string("./src/day02_input.txt").unwrap();
 
-    let mut sum = 0;
-    for line in str.lines() {
-        let split: Vec<usize> = line
+    str.lines().fold(0, |sum, line| {
+        let round: Vec<Mv> = line
             .split_whitespace()
-            .map(|elm| match elm {
-                "A" | "X" => 1,
-                "B" | "Y" => 2,
-                "C" | "Z" => 3,
+            .map(|mv| match mv {
+                "A" | "X" => Mv::Rck,
+                "B" | "Y" => Mv::Ppr,
+                "C" | "Z" => Mv::Scs,
                 _ => unreachable!(),
             })
             .collect();
 
-        let res = match split[..] {
-            [you, me] => match me.cmp(&you) {
-                Ordering::Greater => me + 6,
-                Ordering::Equal => me + 3,
-                Ordering::Less => me + 0,
-            },
-            _ => unreachable!(),
-        };
-
-        sum += res;
-    }
-
-    sum
+        let (elf, me) = (&round[0], &round[1]);
+        sum + elf.against(me) + me.val()
+    })
 }
