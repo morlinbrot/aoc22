93241610edf024c38476d338419d3f3fefe65305
morlinbrot <morlinbrot@mailbox.org>
2022-12-04 23:58:46 +0100

    Day 3

---
+++ src/day03.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+
+fn make_map() -> HashMap<char, usize> {
+    ('a'..='z')
+        .chain('A'..='Z')
+        .enumerate()
+        .map(|(i, c)| (c, i + 1))
+        .collect::<HashMap<char, usize>>()
+}
+
+pub fn part_two() -> usize {
+    let map = make_map();
+    let str = std::fs::read_to_string("./src/day03_input.txt").unwrap();
+    let groups = str.lines().collect::<Vec<&str>>();
+
+    groups.chunks(3).fold(0, |sum, group| {
+        let (one, two, three) = (group[0], group[1], group[2]);
+        let c = one
+            .chars()
+            .find(|&c| two.contains(c) && three.contains(c))
+            .unwrap();
+        sum + map.get(&c).unwrap()
+    })
+}
+
+pub fn part_one() -> usize {
+    let str = std::fs::read_to_string("./src/day03_input.txt").unwrap();
+
+    let map = make_map();
+    str.lines().fold(0, |sum, line| {
+        let (left, right) = line.split_at(line.len() / 2);
+        let c = left.chars().find(|&c| right.contains(c)).unwrap();
+        sum + map.get(&c).unwrap()
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 pub mod day01;
 pub mod day02;
+pub mod day03;
 pub mod util;
 
 pub fn main() {
     // println!("day01 p1: {:?}", day01::part_one());
     // println!("day01 p2: {:?}", day01::part_two());
-    println!("day02 p1: {:?}", day02::part_one());
-    println!("day02 p2: {:?}", day02::part_two());
+    // println!("day02 p1: {:?}", day02::part_one());
+    // println!("day02 p2: {:?}", day02::part_two());
+    println!("day03 p1: {:?}", day03::part_one());
+    println!("day03 p2: {:?}", day03::part_two());
 }
 
 #[cfg(test)]
