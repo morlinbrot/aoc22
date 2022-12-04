57254d8b4995eb74bf42e06d67dea5eca80c6fe9
morlinbrot <morlinbrot@mailbox.org>
2022-12-04 23:58:58 +0100

    Day 4

---
+++ src/day04.rs
@@ -0,0 +1,25 @@
+pub fn part_two() -> usize {
+    let str = std::fs::read_to_string("./src/day04_input").unwrap();
+    str.lines()
+        .map(|line| {
+            line.split([',', '-'])
+                .map(|num| num.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .filter(|nums| !(nums[1] < nums[2] || nums[3] < nums[0]))
+        .count()
+}
+
+pub fn part_one() -> usize {
+    let str = std::fs::read_to_string("./src/day04_input").unwrap();
+    str.lines()
+        .map(|line| {
+            line.split([',', '-'])
+                .map(|num| num.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .filter(|nums| {
+            nums[0] >= nums[2] && nums[1] <= nums[3] || nums[0] <= nums[2] && nums[1] >= nums[3]
+        })
+        .count()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day04;
 pub mod util;
 
 pub fn main() {
@@ -8,8 +9,10 @@ pub fn main() {
     // println!("day01 p2: {:?}", day01::part_two());
     // println!("day02 p1: {:?}", day02::part_one());
     // println!("day02 p2: {:?}", day02::part_two());
-    println!("day03 p1: {:?}", day03::part_one());
-    println!("day03 p2: {:?}", day03::part_two());
+    // println!("day03 p1: {:?}", day03::part_one());
+    // println!("day03 p2: {:?}", day03::part_two());
+    println!("day04 p1: {:?}", day04::part_one());
+    println!("day04 p2: {:?}", day04::part_two());
 }
 
 #[cfg(test)]
