66ebd735a415ec3e8c227fd412cee7f2f35836e8
morlinbrot <morlinbrot@mailbox.org>
2022-12-06 18:44:54 +0100

    Day 6 - use const generics for length param

--- src/d06.rs
+++ src/d06.rs
@@ -1,17 +1,17 @@
 use std::collections::HashSet;
 
-fn find_signal(chars: Vec<char>, len: usize) -> usize {
+fn find_signal<const L: usize>(chars: Vec<char>) -> usize {
     (0..chars.len())
-        .skip(len - 1)
+        .skip(L - 1)
         .find_map(|i| {
             let mut set = HashSet::new();
 
-            for j in 0..len {
+            for j in 0..L {
                 set.insert(chars[i - j]);
             }
 
             match set.len() {
-                l if l == len => Some(i + 1),
+                l if l == L => Some(i + 1),
                 _ => None,
             }
         })
@@ -20,20 +20,20 @@ fn find_signal(chars: Vec<char>, len: usize) -> usize {
 
 pub fn part_two() -> usize {
     let str = std::fs::read_to_string("./src/d06_input").unwrap();
-    let len = 14;
+    const LEN: usize = 14;
     // for line in str.lines() {
-    //     println!("{:?}", find_signal(line.chars().collect(), len));
+    //     println!("{:?}", find_signal::<LEN>(line.chars().collect()));
     // }
 
-    find_signal(str.chars().collect(), len)
+    find_signal::<LEN>(str.chars().collect())
 }
 
 pub fn part_one() -> usize {
     let str = std::fs::read_to_string("./src/d06_input").unwrap();
-    let len = 4;
+    const LEN: usize = 4;
     // for line in str.lines() {
-    //     println!("{:?}", find_signal(line.chars().collect(), len));
+    //     println!("{:?}", find_signal::<LEN>(line.chars().collect()));
     // }
 
-    find_signal(str.chars().collect(), len)
+    find_signal::<LEN>(str.chars().collect())
 }
